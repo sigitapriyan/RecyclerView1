61ab8cc576d9b563dd650f3e207396479cfe8ff3
sigitapriyan <dengansigit@gmail.com>
2016-11-05 22:49:01 +0700

    RecyclerView Grid

--- app/src/main/java/adapter/HotelAdapter.java
+++ app/src/main/java/adapter/HotelAdapter.java
@@ -38,7 +38,6 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
     {
         Hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
-        holder.tvDeskripsi.setText(hotel.deskripsi);
         holder.ivFoto.setImageDrawable(hotel.foto);
     }
 
@@ -54,14 +53,12 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
     {
         ImageView ivFoto;
         TextView tvJudul;
-        TextView tvDeskripsi;
 
         public ViewHolder(View itemView)
         {
             super(itemView);
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
-            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
         }
     }
 
